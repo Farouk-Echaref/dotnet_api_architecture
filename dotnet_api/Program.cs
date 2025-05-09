208392b9764057f04c45d1cf514b0bc7daadd34f
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2025-05-09 17:22:51 +0100

    feat: Post method, createGame dto

--- dotnet_api/Program.cs
+++ dotnet_api/Program.cs
@@ -3,6 +3,8 @@ using dotnet_api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetGameEndpointName = "GetGame";
+
 List<GameDto> games = [
     new (
         1,
@@ -32,9 +34,24 @@ app.MapGet("/fechcha", () => "This is fechcha route");
 app.MapGet("/games", () => games);
 
 // GET /games/{id}
-app.MapGet("/games/{id}", (int id) => games.Find(games => games.Id == id));
+app.MapGet("/games/{id}", (int id) => games.Find(games => games.Id == id))
+    .WithName(GetGameEndpointName);
 
 // POST /games
-// app
+// some validation of the data is required to match the dto 
+app.MapPost("/games", (CreateGameDto incomingGame) => {
+    GameDto newGame =  new GameDto(
+        games.Count + 1,
+        incomingGame.Name,
+        incomingGame.Genre,
+        incomingGame.Price,
+        incomingGame.ReleaseDate
+    );
+
+    games.Add(newGame);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new {id = newGame.Id}, newGame);
+});
+
 app.Run();
 
